94efdfd3e9ce6b60e94bd84652bc9bd6f30d3a19
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-09 23:03:14 +0700

    Add add_from_espeak script

---
+++ add_from_espeak.py
@@ -0,0 +1,37 @@
+#!/bin/python
+
+import subprocess
+from typing import List, Tuple
+
+import argparse
+parser=argparse.ArgumentParser(
+		usage="Append to open-dict-additional.txt, use espeak to generate the pronunciation",
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("words", help="List of words", nargs="*")
+args=parser.parse_args()
+words: List[str]=[*dict.fromkeys(args.words)]
+if not words: exit()
+
+pronounces: List[str]=subprocess.check_output([
+	"espeak",
+	"-q",
+	"--ipa",
+	' '.join(words)
+	]).decode('u8').split()
+
+
+with open("open-dict-additional.txt", "a") as f:
+	for word, pronounce in zip(words, pronounces):
+		pronounce=pronounce.replace("əʊ", "oʊ").replace("ɜː", "əɹ").replace("eə", "ɛɹ"
+				).translate({
+			ord("l"): "ɫ",
+			ord("r"): "ɹ",
+			ord("ɐ"): "ə",
+			ord("ɒ"): "ɑ",
+			ord("ʌ"): "ə",
+			ord("a"): "æ",  # temporary
+			ord("ː"): "",
+			}).replace("æɪ", "aɪ")
+		assert not ({*pronounce}&{'l', 'r', 'ɐ', 'ɒ', 'ɜ', 'ʌ', 'ː'}), pronounce
+		assert "əʊ" not in pronounce, pronounce
+		f.write(f"{word}\t/{pronounce}/\n")
