198d99ce1afd2059809a58cc5c333db2a90023a6
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-10 09:07:59 +0700

    Allow lookup_part_pronunciation to print multiple lines per entry

--- lookup_part_pronunciation.py
+++ lookup_part_pronunciation.py
@@ -5,6 +5,7 @@ import local_lib
 from lib import*
 from lib_steno import*
 import itertools
+import textwrap
 
 default_input_files=[tempdir/"out"]
 
@@ -15,6 +16,10 @@ parser=argparse.ArgumentParser(
 parser.add_argument("-i", "--input", type=Path, action="append",
 		help="Path to input file (matched pronunciation dictionary). Can be specified multiple times. "
 		f"If not specified, default to {default_input_files}.")
+parser.add_argument("-w", "--width", type=int, default=100,
+		help="Maximum row length.")
+parser.add_argument("-l", "--lines", type=int, default=2,
+		help="Maximum number of lines per pronunciation.")
 parser.add_argument("query",
 		help="Regular expression to match the spelling.")
 args=parser.parse_args()
@@ -42,4 +47,9 @@ for m in items:
 
 max_pronounce_len=max(len(pronounce) for pronounce in d.items())
 for pronounce, words in sorted(d.items(), key=lambda x: len(x[1]), reverse=True):
-	print(f"{pronounce:{max_pronounce_len}}  {len(words):5}  {str(words)[:100]}")
+	prefix=f"{pronounce:{max_pronounce_len}}  {len(words):5}  "
+	lines=textwrap.wrap(' '.join(words), args.width)[:args.lines]
+	print(prefix+lines[0])
+	prefix=' '*len(prefix)
+	for line in lines[1:]:
+		print(prefix+line)
