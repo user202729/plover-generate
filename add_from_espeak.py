466a7faaf7c4ced4709a9c6dde5ac405867d1a7d
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-13 15:59:03 +0700

    Add --raw flag to add_from_espeak script

--- add_from_espeak.py
+++ add_from_espeak.py
@@ -10,6 +10,7 @@ parser=argparse.ArgumentParser(
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("words", help="List of words", nargs="*")
 parser.add_argument("-n", "--dry-run", help="Print to stdout instead.", action="store_true")
+parser.add_argument("-r", "--raw", help="Print in espeak's raw IPA output format.", action="store_true")
 args=parser.parse_args()
 words: List[str]=[*dict.fromkeys(args.words)]
 if not words: exit()
@@ -21,19 +22,21 @@ pronounces: List[str]=subprocess.check_output([
 	' '.join(words)
 	]).decode('u8').split()
 
+if args.raw: assert args.dry_run, "Cannot use --raw without --dry-run"
 
 with (sys.stdout if args.dry_run else open("open-dict-additional.txt", "a")) as f:
 	for word, pronounce in zip(words, pronounces):
-		pronounce=pronounce.replace("əʊ", "oʊ").replace("ɜː", "əɹ").replace("eə", "ɛɹ"
-				).translate({
-			ord("l"): "ɫ",
-			ord("r"): "ɹ",
-			ord("ɐ"): "ə",
-			ord("ɒ"): "ɑ",
-			ord("ʌ"): "ə",
-			ord("a"): "æ",  # temporary
-			ord("ː"): "",
-			}).replace("æɪ", "aɪ").replace("æʊ", "aʊ")
-		assert not ({*pronounce}&{'l', 'r', 'ɐ', 'ɒ', 'ɜ', 'ʌ', 'ː'}), pronounce
-		assert "əʊ" not in pronounce, pronounce
+		if not args.raw:
+			pronounce=pronounce.replace("əʊ", "oʊ").replace("ɜː", "əɹ").replace("eə", "ɛɹ"
+					).translate({
+				ord("l"): "ɫ",
+				ord("r"): "ɹ",
+				ord("ɐ"): "ə",
+				ord("ɒ"): "ɑ",
+				ord("ʌ"): "ə",
+				ord("a"): "æ",  # temporary
+				ord("ː"): "",
+				}).replace("æɪ", "aɪ").replace("æʊ", "aʊ")
+			assert not ({*pronounce}&{'l', 'r', 'ɐ', 'ɒ', 'ɜ', 'ʌ', 'ː'}), pronounce
+			assert "əʊ" not in pronounce, pronounce
 		f.write(f"{word}\t/{pronounce}/\n")
