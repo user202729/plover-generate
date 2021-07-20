8b7abdbc985ee4d18b38b07dd391f67dc529b1bb
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-20 11:55:29 +0700

    Add disambiguation_stroke argument to word boundary conflicts script

--- word_boundary_conflicts.py
+++ word_boundary_conflicts.py
@@ -11,6 +11,8 @@ parser=argparse.ArgumentParser(usage="Determine word boundary conflicts")
 parser.add_argument("dictionary", type=Path, help="Path to JSON dictionary.")
 parser.add_argument("ngrams_file", type=Path, nargs="?", default=Path("bigram-frequency.json"),
 		help="Path to n-grams file. Must be a JSON file that contains an iterable")
+parser.add_argument("--disambiguation-stroke", type=str, default="R-RPB",
+		help=f"Stroke to disambiguate conflicts. Entries that end with this stroke will not be included")
 args=parser.parse_args()
 
 Outline=Tuple[str, ...]
@@ -19,6 +21,13 @@ dictionary: Dict[Outline, str]={
 		for outline, translation in
 			json.loads(args.dictionary.read_text())
 			.items()}
+if args.disambiguation_stroke:
+	disambiguation_stroke: str=args.disambiguation_stroke
+	dictionary={
+			outline: translation
+			for outline, translation in dictionary.items()
+			if outline[-1]!=disambiguation_stroke}
+
 word_to_strokes: Dict[str, List[Outline]]={
 		word: [item[0] for item in items]
 		for word, items in
