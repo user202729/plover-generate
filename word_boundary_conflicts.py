b0a3a69267f95b7099562f6c41ba881e67c61677
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-20 11:19:45 +0700

    Add word boundary conflicts script

---
+++ word_boundary_conflicts.py
@@ -0,0 +1,49 @@
+#!/bin/python
+
+from time import time
+start_time=time()
+
+from pathlib import Path
+from lib import *
+import json
+import argparse
+parser=argparse.ArgumentParser(usage="Determine word boundary conflicts")
+parser.add_argument("dictionary", type=Path, help="Path to JSON dictionary.")
+parser.add_argument("ngrams_file", type=Path, nargs="?", default=Path("bigram-frequency.json"),
+		help="Path to n-grams file. Must be a JSON file that contains an iterable")
+args=parser.parse_args()
+
+Outline=Tuple[str, ...]
+dictionary: Dict[Outline, str]={
+		tuple(outline.split("/")): translation
+		for outline, translation in
+			json.loads(args.dictionary.read_text())
+			.items()}
+word_to_strokes: Dict[str, List[Outline]]={
+		word: [item[0] for item in items]
+		for word, items in
+		group_sort(dictionary.items(), key=lambda x: x[1])
+		}
+
+def process_ngram(ngram: List[str])->None:
+	if len(ngram)==1: return
+	try:
+		entries=itertools.product(*(word_to_strokes[word] for word in ngram))
+	except KeyError:
+		return
+	entry: Tuple[Outline, ...]
+	for entry in entries:
+		cat: Outline=sum(entry, ())
+		for i in range(len(entry[0])+1, len(cat)+1):
+			if cat[:i] in dictionary:
+				expected_output: str=' '.join(ngram)
+				actual_output: str=dictionary[cat[:i]]
+				if expected_output!=actual_output:
+					print(f"Conflict: {expected_output!r} -> {actual_output!r}" +
+							('' if i==len(cat) else ' [...]'))
+					return  # only need to print one instance
+				else:
+					pass  # intentional multiple word definition
+
+for ngram in json.loads(args.ngrams_file.read_text()):
+	process_ngram(ngram.split())
